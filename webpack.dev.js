927185a4b476fb870291cd84716d9b127b0556f2
Paulina Nowek <nowek.paulina@gmail.com>
2021-05-14 19:47:21 +0200

    server update

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,7 +17,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
+           }
         ]
     },
     plugins: [
